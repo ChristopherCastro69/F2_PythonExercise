2569635e84b4b0af50a18d5aab32d827dce29884
Topitops69 <topitops123@gmail.com>
2023-09-04 14:49:24 +0800

    Final Answer

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ while True:
         num2 = int(input("Enter the second integer: "))
         num3 = int(input("Enter the third integer: "))
 
-        # Find the maximum among them using conditional statements
         max_value = num1
 
         if num2 > max_value:
@@ -87,17 +86,17 @@ while True:
         def decToBinary(dec):
             if dec == 0:
                 return '0b0'  # Special case for 0
-            binary = ''  # Initialize an empty string to store the binary representation
+            binary = ''
             while dec > 0:
-                binary = str(dec % 2) + binary  # Append the remainder to the left of the binary string
+                binary = str(dec % 2) + binary
                 dec //= 2
-            return '0b' + binary  # Add '0b' prefix to indicate binary
+            return '0b' + binary
 
         # Function to convert binary to another base (octal or hexadecimal)
         def binaryToN(binary, base_type):
             decimal = 0
             power = 0
-            base = 8 if base_type == 'octal' else 16  # Set the base to 8 for octal, 16 for hexadecimal
+            base = 8 if base_type == 'octal' else 16
             for digit in binary[::-1]:
                 decimal += int(digit) * (base ** power)
                 power += 1
@@ -107,21 +106,21 @@ while True:
         def decToOctal(dec):
             if dec == 0:
                 return '0o0'  # Special case for 0
-            octal = ''  # Initialize an empty string to store the octal representation
+            octal = ''
             while dec > 0:
-                octal = str(dec % 8) + octal  # Append the remainder to the left of the octal string
+                octal = str(dec % 8) + octal
                 dec //= 8
-            return '0o' + octal  # Add '0o' prefix to indicate octal
+            return '0o' + octal
 
         # Function to convert decimal to hexadecimal
         def decToHex(dec):
             if dec == 0:
                 return '0x0'  # Special case for 0
             hex_chars = '0123456789ABCDEF'
-            hexadecimal = ''  # Initialize an empty string to store the hexadecimal representation
+            hexadecimal = ''
             while dec > 0:
                 remainder = dec % 16
-                hexadecimal = hex_chars[remainder] + hexadecimal  # Append the hex character to the left
+                hexadecimal = hex_chars[remainder] + hexadecimal
                 dec //= 16
             return '0x' + hexadecimal  # Add '0x' prefix to indicate hexadecimal
 
